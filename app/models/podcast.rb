345c3d8c76218e7061128bec458c33f2f2de5295
Alexander Abramov <omgout.200@gmail.com>
2024-02-07 22:10:36 +0700

    created basic specs for podcast model

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,3 +1,5 @@
 class Podcast < ApplicationRecord
   has_one_attached :audio
+
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Podcast, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:podcast) { FactoryBot.build(:podcast) }
+
+  it 'is valid with FactoryBot attributes' do
+    expect(podcast).to be_valid
+  end
+
+  describe 'validations' do
+    context 'name' do
+      it 'is invalid when nil' do
+        podcast.name = nil
+        expect(podcast).to_not be_valid
+      end
+
+      it 'is invalid with non-unique name' do
+        podcast.save
+        failed_podcast = Podcast.new(name: podcast.name)
+
+        expect(failed_podcast).to_not be_valid
+      end
+    end
+
+    context 'body' do
+      it 'is valid when nil' do
+        podcast.body = nil
+        expect(podcast).to be_valid
+      end
+
+      it 'is valid with a unique body' do
+        podcast.save
+        new_podcast = FactoryBot.build(:podcast)
+        new_podcast.body = podcast.body
+
+        expect(new_podcast).to be_valid
+      end
+    end
+  end
 end

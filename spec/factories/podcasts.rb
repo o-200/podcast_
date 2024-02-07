f72677c5cdc0ad7b3b7a0b88b6bb8effe0cb49dd
Alexander Abramov <omgout.200@gmail.com>
2024-02-07 22:20:45 +0700

    added testfile, connect them to attach

--- spec/factories/podcasts.rb
+++ spec/factories/podcasts.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :podcast do
     sequence (:name) { |n| "name #{n}"}
     body { Faker::Quotes::Chiquito.joke }
+    audio { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'testfiles', 'testfile.mp3'), 'audio/mpeg') }
   end
 end
